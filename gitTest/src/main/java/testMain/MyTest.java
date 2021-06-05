fb7d08120a46b4b37823f3ef5895f25629ff8c8a
AKePeng <1946229299@qq.com>
2021-06-05 13:47:12 +0800

    回退

--- gitTest/src/main/java/testMain/MyTest.java
+++ gitTest/src/main/java/testMain/MyTest.java
@@ -3,5 +3,8 @@ package testMain;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git测试");
+
+
+        System.out.println("试试能不能回退");
     }
 }
